5557742d4040882acbf94b54310c268b10ed4cfe
Joost Molenaar <jjm@j0057.nl>
2019-12-01 21:58:23 +0100

    Make puzzle() more flexible
    The previous version was hardcoded for the parse function to return
    Vec<T>, now it can return anything as long as it can be dereferenced
    into a &U. The ?Sized constraint on U took some reading[1] and the
    '&*input' took some experimenting.
    
    [1] https://doc.rust-lang.org/book/ch19-04-advanced-types.html#dynamically-sized-types-and-the-sized-trait

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 mod util;
 mod day01;
 
+use std::ops::Deref;
+use std::fmt::Display;
 use std::error::Error;
 
-fn puzzle<T, A>(day: u8,
-                part: char,
-                parse: Box<dyn Fn(&str) -> Result<Vec<T>, Box<dyn Error>>>,
-                solve: Box<dyn Fn(&[T]) -> A>)
-               -> Result<(), Box<dyn Error>>
-               where A: std::fmt::Display {
-    let input: Vec<T> = parse(&format!("input/day{:02}.txt", day))?;
+fn puzzle<T: Deref<Target=U>, U: ?Sized, A: Display>(
+        day: u8,
+        part: char,
+        parse: Box<dyn Fn(&str) -> Result<T, Box<dyn Error>>>,
+        solve: Box<dyn Fn(&U) -> A>)
+        -> Result<(), Box<dyn Error>> {
+    let input = parse(&format!("input/day{:02}.txt", day))?;
     let start = std::time::SystemTime::now();
-    let answer: A = solve(&input);
+    let answer = solve(&*input);
     let elapsed = start.elapsed()?.as_micros();
     println!("problem: {:>2}{}; time: {:9} μs; answer: {}", day, part, elapsed, answer);
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-
     println!("Advent of Code 2019, by j0057");
 
-    puzzle::<u32, u32>(1, 'A', Box::new(util::get_numbers::<u32>), Box::new(day01::day01a))?;
-    puzzle::<u32, u32>(1, 'B', Box::new(util::get_numbers::<u32>), Box::new(day01::day01b))?;
+    puzzle(1, 'A', Box::new(util::get_numbers::<u32>), Box::new(day01::day01a))?;
+    puzzle(1, 'B', Box::new(util::get_numbers::<u32>), Box::new(day01::day01b))?;
 
     Ok(())
 }
