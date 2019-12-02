c97c1523571c0e2b0a35b264ef570cac3096bb80
Joost Molenaar <jjm@j0057.nl>
2019-12-02 06:09:38 +0100

    Fix code style

--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,19 @@
 use std::io::BufRead;
+use std::error::Error;
+use std::str::FromStr;
+use std::num::ParseIntError;
 
-pub fn get_lines(filename: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+pub fn get_lines(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let file = std::fs::File::open(filename)?;
     let reader = std::io::BufReader::new(file);
     let result = reader.lines().collect::<Result<Vec<String>, std::io::Error>>()?;
     Ok(result)
 }
 
-pub fn get_numbers<T>(filename: &str) -> Result<Vec<T>, Box<dyn std::error::Error>>
-where T: std::str::FromStr<Err=std::num::ParseIntError> {
+pub fn get_numbers<T: FromStr<Err=ParseIntError>>(filename: &str) -> Result<Vec<T>, Box<dyn Error>> {
     let result = get_lines(filename)?
         .iter()
         .map(|s| s.parse::<T>())
-        .collect::<Result<Vec<T>, std::num::ParseIntError>>()?;
+        .collect::<Result<Vec<T>, ParseIntError>>()?;
     Ok(result)
 }
