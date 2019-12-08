77f865667c9204df833c37fa6dff781827e21915
Joost Molenaar <jjm@j0057.nl>
2019-12-08 21:52:03 +0100

    [03] Crossed Wires: add unit tests for part one

--- src/day03.rs
+++ src/day03.rs
@@ -109,7 +109,7 @@ mod test {
     }
 
     #[test]
-    fn test_03_ex0_err() {
+    fn test_03_ex0_err_1() {
         let result = "R8,U5,L5,X3".parse::<super::Input>();
         assert!(result.is_err(), "expected error result");
     }
@@ -131,6 +131,26 @@ mod test {
             .map(|&(y, x)| Complex::<i64>::new(x, y))
             .collect::<HashSet<_>>();
         assert_eq!(path, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_03_ex2() -> Result<(), Box<dyn Error>> {
+        let input = ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]
+            .iter()
+            .map(|s| s.parse::<super::Input>())
+            .collect::<Result<Vec<_>, super::InputError>>()?;
+        assert_eq!(super::day03a(&input), 159);
+        Ok(())
+    }
+
+    #[test]
+    fn test_03_ex3() -> Result<(), Box<dyn Error>> {
+        let input = ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]
+            .iter()
+            .map(|s| s.parse::<super::Input>())
+            .collect::<Result<Vec<_>, super::InputError>>()?;
+        assert_eq!(super::day03a(&input), 135);
         Ok(())
     }
 
