203aef52b639a49c9f11d9dccdafa71983f59555
Joost Molenaar <jjm@j0057.nl>
2019-12-10 12:48:42 +0100

    [05] Sunny with a Chance of Asteroids: add unit tests for part one

--- src/day05.rs
+++ src/day05.rs
@@ -12,7 +12,26 @@ pub fn day05a(program: &[i128]) -> i128 {
 mod test {
     use std::error::Error;
 
+    use crate::day02;
     use crate::util;
+
+    #[test]
+    fn test_05_ex1() { // outputs whatever it gets as input, then halts
+        let mut program = vec![3, 0, 4, 0, 99];
+        let mut input = vec![1234567890];
+        let mut output = vec![];
+        day02::run(&mut program, &mut input, &mut output);
+        assert_eq!(output, &[1234567890]);
+    }
+
+    #[test]
+    fn test_05_ex2() { // multiply using position and immediate mode
+        let mut program = vec![1002, 4, 3, 4, 33];
+        let mut input = vec![];
+        let mut output = vec![];
+        day02::run(&mut program, &mut input, &mut output);
+        assert_eq!(program, &[1002, 4, 3, 4, 99]);
+    }
 
     #[test]
     fn test_05() -> Result<(), Box<dyn Error>> {
