af1c8796b32fff781fe443a2cdfc292a319f3373
Joost Molenaar <jjm@j0057.nl>
2019-12-31 11:12:37 +0100

    [14] Space Stoichiometry: basic parser
    "stoichiometry, n. -- Calculation of the quantities of reactants and
    products in a chemical reaction."

---
+++ src/day14.rs
@@ -0,0 +1,89 @@
+//
+// enum InputError
+//
+
+#[derive(Debug, thiserror::Error)]
+pub enum InputError {
+    #[error("Error parsing text: {0:?}")]
+    Parse(Option<String>),
+
+    #[error("Error parsing number: {0:?}")]
+    ParseInt(#[from] std::num::ParseIntError),
+}
+
+//
+// struct Reagent
+//
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Reagent {
+    amount: i64,
+    name: String
+}
+
+impl std::str::FromStr for Reagent {
+    type Err = InputError;
+
+    fn from_str(text: &str) -> Result<Self, Self::Err> {
+        let parts = text.split(' ').collect::<Vec<_>>();
+        if parts.len() != 2 {
+            return Err(InputError::Parse(Some(text.to_owned())));
+        }
+        let result = Reagent {
+            amount: parts[0].parse()?,
+            name: parts[1].to_owned()
+        };
+        Ok(result)
+    }
+}
+
+//
+// struct Reaction
+//
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Reaction {
+    precursors: Vec<Reagent>,
+    product: Reagent,
+}
+
+impl std::str::FromStr for Reaction {
+    type Err = InputError;
+
+    fn from_str(text: &str) -> Result<Self, Self::Err> {
+        let parts = text.split(" => ").collect::<Vec<_>>();
+        if parts.len() != 2 {
+            return Err(InputError::Parse(Some(text.to_owned())));
+        }
+        let result = Reaction {
+            precursors: parts[0].split(", ").map(|s| s.parse::<Reagent>()).collect::<Result<Vec<Reagent>, InputError>>()?,
+            product: parts[1].parse::<Reagent>()?,
+        };
+        Ok(result)
+    }
+}
+
+//
+// tests
+//
+
+#[cfg(test)]
+mod test {
+    use std::error::Error;
+
+    use crate::util;
+
+    #[test]
+    fn test_14_ex1() -> Result<(), Box<dyn Error>> {
+        let input: Vec<super::Reaction>
+                  = util::parse_lines("10 ORE => 10 A\n\
+                                       1 ORE => 1 B\n\
+                                       7 A, 1 B => 1 C\n\
+                                       7 A, 1 C => 1 D\n\
+                                       7 A, 1 D => 1 E\n\
+                                       & A, 1 E => 1 FUEL")?;
+        assert_eq!(input[0], super::Reaction { precursors: vec![super::Reagent { amount: 10, name: "ORE".to_owned() }],
+                                               product: super::Reagent { amount: 10, name: "A".to_owned() } });
+        Ok(())
+    }
+}
