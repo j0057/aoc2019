3dcd67d2fd9247132b6780665fe0c4fafb9eca94
Joost Molenaar <jjm@j0057.nl>
2020-01-01 21:55:57 +0100

    [14] Space Stoichiometry: factor out test inputs

--- src/day14.rs
+++ src/day14.rs
@@ -150,14 +150,58 @@ mod test {
                         7 A, 1 D => 1 E\n\
                         7 A, 1 E => 1 FUEL";
 
+    static EX2: &str = "9 ORE => 2 A\n\
+                        8 ORE => 3 B\n\
+                        7 ORE => 5 C\n\
+                        3 A, 4 B => 1 AB\n\
+                        5 B, 7 C => 1 BC\n\
+                        4 C, 1 A => 1 CA\n\
+                        2 AB, 3 BC, 4 CA => 1 FUEL";
+
+    static EX3: &str = "157 ORE => 5 NZVS\n\
+                        165 ORE => 6 DCFZ\n\
+                        44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL\n\
+                        12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ\n\
+                        179 ORE => 7 PSHF\n\
+                        177 ORE => 5 HKGWZ\n\
+                        7 DCFZ, 7 PSHF => 2 XJWVT\n\
+                        165 ORE => 2 GPVTF\n\
+                        3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT";
+
+    static EX4: &str = "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG\n\
+                        17 NVRVD, 3 JNWZP => 8 VPVL\n\
+                        53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL\n\
+                        22 VJHF, 37 MNCFX => 5 FWMGM\n\
+                        139 ORE => 4 NVRVD\n\
+                        144 ORE => 7 JNWZP\n\
+                        5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC\n\
+                        5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV\n\
+                        145 ORE => 6 MNCFX\n\
+                        1 NVRVD => 8 CXFTF\n\
+                        1 VJHF, 6 MNCFX => 4 RFSQX\n\
+                        176 ORE => 6 VJHF";
+
+    static EX5: &str = "171 ORE => 8 CNZTR\n\
+                        7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL\n\
+                        114 ORE => 4 BHXH\n\
+                        14 VRPVC => 6 BMBT\n\
+                        6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL\n\
+                        6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT\n\
+                        15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW\n\
+                        13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW\n\
+                        5 BMBT => 4 WPTQ\n\
+                        189 ORE => 9 KTJDG\n\
+                        1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP\n\
+                        12 VRPVC, 27 CNZTR => 2 XDBXC\n\
+                        15 KTJDG, 12 BHXH => 5 XCVML\n\
+                        3 BHXH, 2 VRPVC => 7 MZWV\n\
+                        121 ORE => 7 VRPVC\n\
+                        7 XCVML => 6 RJRHP\n\
+                        5 BHXH, 4 VRPVC => 5 LTCX";
+
     #[test]
     fn test_14_ex1() -> Result<(), Box<dyn Error>> {
-        let input = util::parse_lines("10 ORE => 10 A\n\
-                                       1 ORE => 1 B\n\
-                                       7 A, 1 B => 1 C\n\
-                                       7 A, 1 C => 1 D\n\
-                                       7 A, 1 D => 1 E\n\
-                                       7 A, 1 E => 1 FUEL")?;
+        let input = util::parse_lines(EX1)?;
         assert_eq!(input[0], super::Reaction::new(vec![super::Reagent::new(10, "ORE")], super::Reagent::new(10, "A")));
         let mut reactor = super::Reactor::new(&input);
         assert_eq!(reactor.ore_cost(1, "FUEL"), 31);
@@ -167,74 +211,29 @@ mod test {
 
     #[test]
     fn test_14_ex2() -> Result<(), Box<dyn Error>> {
-        let input = util::parse_lines("9 ORE => 2 A\n\
-                                       8 ORE => 3 B\n\
-                                       7 ORE => 5 C\n\
-                                       3 A, 4 B => 1 AB\n\
-                                       5 B, 7 C => 1 BC\n\
-                                       4 C, 1 A => 1 CA\n\
-                                       2 AB, 3 BC, 4 CA => 1 FUEL")?;
-        let mut reactor = super::Reactor::new(&input);
-        assert_eq!(reactor.ore_cost(1, "FUEL"), 165);
+        let input = util::parse_lines(EX2)?;
+        assert_eq!(super::day14a(&input), 165);
         Ok(())
     }
 
     #[test]
     fn test_14_ex3() -> Result<(), Box<dyn Error>> {
-        let input = util::parse_lines("157 ORE => 5 NZVS\n\
-                                       165 ORE => 6 DCFZ\n\
-                                       44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL\n\
-                                       12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ\n\
-                                       179 ORE => 7 PSHF\n\
-                                       177 ORE => 5 HKGWZ\n\
-                                       7 DCFZ, 7 PSHF => 2 XJWVT\n\
-                                       165 ORE => 2 GPVTF\n\
-                                       3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT")?;
-        let mut reactor = super::Reactor::new(&input);
-        assert_eq!(reactor.ore_cost(1, "FUEL"), 13312);
+        let input = util::parse_lines(EX3)?;
+        assert_eq!(super::day14a(&input), 13_312);
         Ok(())
     }
 
     #[test]
     fn test_14_ex4() -> Result<(), Box<dyn Error>> {
-        let input = util::parse_lines("2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG\n\
-                                       17 NVRVD, 3 JNWZP => 8 VPVL\n\
-                                       53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL\n\
-                                       22 VJHF, 37 MNCFX => 5 FWMGM\n\
-                                       139 ORE => 4 NVRVD\n\
-                                       144 ORE => 7 JNWZP\n\
-                                       5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC\n\
-                                       5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV\n\
-                                       145 ORE => 6 MNCFX\n\
-                                       1 NVRVD => 8 CXFTF\n\
-                                       1 VJHF, 6 MNCFX => 4 RFSQX\n\
-                                       176 ORE => 6 VJHF")?;
-        let mut reactor = super::Reactor::new(&input);
-        assert_eq!(reactor.ore_cost(1, "FUEL"), 180_697);
+        let input = util::parse_lines(EX4)?;
+        assert_eq!(super::day14a(&input), 180_697);
         Ok(())
     }
 
     #[test]
     fn test_14_ex5() -> Result<(), Box<dyn Error>> {
-        let input = util::parse_lines("171 ORE => 8 CNZTR\n\
-                                       7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL\n\
-                                       114 ORE => 4 BHXH\n\
-                                       14 VRPVC => 6 BMBT\n\
-                                       6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL\n\
-                                       6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT\n\
-                                       15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW\n\
-                                       13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW\n\
-                                       5 BMBT => 4 WPTQ\n\
-                                       189 ORE => 9 KTJDG\n\
-                                       1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP\n\
-                                       12 VRPVC, 27 CNZTR => 2 XDBXC\n\
-                                       15 KTJDG, 12 BHXH => 5 XCVML\n\
-                                       3 BHXH, 2 VRPVC => 7 MZWV\n\
-                                       121 ORE => 7 VRPVC\n\
-                                       7 XCVML => 6 RJRHP\n\
-                                       5 BHXH, 4 VRPVC => 5 LTCX")?;
-        let mut reactor = super::Reactor::new(&input);
-        assert_eq!(reactor.ore_cost(1, "FUEL"), 2_210_736);
+        let input = util::parse_lines(EX5)?;
+        assert_eq!(super::day14a(&input), 2_210_736);
         Ok(())
     }
 
